b9f94ba01b8c944b55b766cc5361319755316767
yac <yac@blesmrt.net>
2012-05-30 02:22:04 +0200

    support multiple sources
    *	also ls command

--- src/usr/lib/ydu/commands/radio.sh
+++ src/usr/lib/ydu/commands/radio.sh
@@ -11,14 +11,38 @@ usage() {
 	echo "Usage: <radio_to_play>"
 }
 
-main() {
-	local radio line
+play() {
+	local radio line index
+	local -a lines
 
 	radio=$1
+	index=${2:-0}
+
+	line=`cat $RADIO_DATA | grep -E "^$radio\|"` || die undefined radio
+	lines=( `echo ${line#*|} | sed 's/|/ /g'` )
+
+	[[ $index -ge ${#lines[@]} ]] && die undefined index
+	[[ -z "${#lines[$index]}" ]] && die undefined value
+
+	echo "playing ${lines[$index]}"
+	$RADIO_PLAYER ${lines[$index]}
+}
+
+list() {
+	local lines indexes
+	lines=`cat $RADIO_DATA`
+	for i in $lines; do
+		indexes=$(( `echo $i | sed 's/[^|]//g' | wc -c`-1 ))
+		echo $indexes ${i%%|*}
+	done
+}
+
+main() {
 	source $HOME/.config/ydu/radio.sh
 
-	line=`cat $RADIO_DATA | grep -E "^$radio\|"` || die "no such radio defined"
-	line=`echo $line | sed 's/^.*|\(.*\)$/\1/'`
-	echo "playing $line"
-	$RADIO_PLAYER $line
+	if [[ $1 == "ls" ]] ; then
+		list
+	else
+		play $@
+	fi
 }
