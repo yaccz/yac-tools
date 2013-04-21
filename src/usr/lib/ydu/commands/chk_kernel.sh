d30f94e603aa610056594c535912930afa2fa195
yac <yac@blesmrt.net>
2013-04-21 20:37:50 +0200

    + kernel checker

---
+++ src/usr/lib/ydu/commands/chk_kernel.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+set -eu
+
+usage() {
+	echo "Usage: $0 <group-to-check>"
+}
+
+main() {
+	. ~/.config/ydu/chk_kernel.sh
+
+	opts_$1
+	shift
+
+	for i in ${OPTS[@]}; do
+		grep $i $CONFIG || echo "$i not found"
+	done
+}
