d86380c2478986af51007fc8961913f23d9988e7
Jan (yac) Matějka <JMatejka@suse.cz>
2015-03-02 14:45:56 +0100

    Add command: addrinfo

--- src/ypt.hs
+++ src/ypt.hs
@@ -1,12 +1,14 @@
 import System.Environment
 import Ypt.Types
 import qualified Ypt.Commands.FindGitDirs as FGD
+import qualified Ypt.Commands.Addrinfo as AI
 
 
 dispatchCommand :: [String] -> IO ()
 dispatchCommand []                   = errorMsg MissingCommand
 
 dispatchCommand ("find-git-dirs":xs) = FGD.main xs
+dispatchCommand ("addrinfo":xs)      = AI.main xs
 
 dispatchCommand (x:_)                = errorMsg $ CmdNotFound x
 
