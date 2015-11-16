65c9e168db6b2fe0233bd9a6d7fd02b5d2bd41d2
Jan Matějka <jmatejka@suse.cz>
2015-11-16 18:02:51 +0100

    Register blkdev 253 : fake-blkdev

--- src/linux-mod-fake-blkdev/fake-blkdev.c
+++ src/linux-mod-fake-blkdev/fake-blkdev.c
@@ -1,13 +1,27 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+
+int MAJOR = 253;
+char* NAME = "fake-blkdev";
 
 int init_module(void)
 {
-    printk(KERN_INFO "fake-blkdev initialized\n");
+    int ret;
+
+    ret = register_blkdev(MAJOR, NAME);
+    if ( ret < 0 ) {
+        printk(KERN_INFO "fake-blkdev: register_blkdev failed\n");
+        return 1;
+    }
+
+    printk(KERN_INFO "fake-blkdev: major=%d name=%s\n", MAJOR, NAME);
+    printk(KERN_INFO "fake-blkdev: initialized\n");
     return 0;
 }
 
 void cleanup_module(void)
 {
-    printk(KERN_INFO "fake-blkdev cleaned up\n");
+    unregister_blkdev(MAJOR, NAME);
+    printk(KERN_INFO "fake-blkdev: cleaned up\n");
 }
